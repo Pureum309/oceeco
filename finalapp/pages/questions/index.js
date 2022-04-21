ea13261a02be9d8c866ade064eff4f315bf0a4f1
Pureum309 <psun27@my.bcit.ca>
2022-04-20 20:44:56 -0700

    finalized button css

--- finalapp/pages/questions/index.js
+++ finalapp/pages/questions/index.js
@@ -6,6 +6,8 @@ import styles from '../../styles/Home.module.css';
 import Options from '../../comps/Questions/options';
 import {GetName, qs} from "../../data/que_content";
 
+
+
 export default function Questions() {
 
     const nm= GetName();
@@ -38,34 +40,32 @@ export default function Questions() {
     <div className={styles.container}>
 
     {/* Start from Heare Sarah */}
+        <Options
+          q={qs[qnum].title}
+          img={qs[qnum].options[opt].pics}
+          desc={qs[qnum].options[opt].txts}
+          background={qs[qnum].background}
+        />
+    
+    {/* When reach last question hide the button */}
+    {/* {
+      Number(qnum) < qs.length-1 &&
+      <button onClick={
+        ()=> r.push({
+          pathname:"questions",
+          query:{
+            qnum:Number(qnum)+1 >= qs.length ? qs.length -1 : Number(qnum)+1,
+          }
+        })
+      }>Next</button>
+    } */}
 
-    <Options
-      q={qs[qnum].title}
-      img={qs[qnum].options[opt].pics}
-      desc={qs[qnum].options[opt].txts}
-      background={qs[qnum].background}
-    />
-  
-  {/* When reach last question hide the button */}
-  {/* {
-    Number(qnum) < qs.length-1 &&
-    <button onClick={
-      ()=> r.push({
-        pathname:"questions",
-        query:{
-          qnum:Number(qnum)+1 >= qs.length ? qs.length -1 : Number(qnum)+1,
-        }
-      })
-    }>Next</button>
-  } */}
-
-  {/* {
-    Number(qnum) === qs.length -1 &&
-    <button onClick={
-      ()=>r.push("/questions/result")
-    }>Finish!!</button>
-  } */}
-
+    {/* {
+      Number(qnum) === qs.length -1 &&
+      <button onClick={
+        ()=>r.push("/questions/result")
+      }>Finish!!</button>
+    } */}
       
     </div>
   )

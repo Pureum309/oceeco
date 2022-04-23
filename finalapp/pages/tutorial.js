4b0a453e76a8c45bf890fcf36b1e4bbab8bdfb02
Heidingg <tnguyen480@my.bcit.ca>
2022-04-22 20:16:44 -0700

    Edit the tutorial page + add button

--- finalapp/pages/tutorial.js
+++ finalapp/pages/tutorial.js
@@ -1,105 +1,226 @@
 import {useRouter} from "next/router";
 import styles from '../styles/Home.module.css';
 import Script from 'next/script'
+import $ from 'jquery';
 export default function tutorial() {
-
     const r = useRouter();
- 
+    if (process.browser) {
+        var aScript = document.createElement('script');
+        aScript.type = 'text/javascript';
+        aScript.src = " https://js.stripe.com/v3/";
+        document.head.appendChild(aScript);
+        aScript.onload = () => {
+            $(document).ready(function(){
+                $("#divs div").each(function(e) {
+                    if (e != 0)
+                        $(this).hide();
+                });
+                $("#next").click(function(){
+                    if ($("#divs div:visible").next().length != 0)
+                        $("#divs div:visible").next().show().prev().hide();
+                    else {
+                        $("#divs div:visible").hide();
+                        $("#divs div:first").show();
+                    }
+                    return false;
+                });
+                $("#prev").click(function(){
+                  if ($("#divs div:visible").prev().length != 0)
+                      $("#divs div:visible").prev().show().next().hide();
+                  else {
+                      $("#divs div:visible").hide();
+                      $("#divs div:last").show();
+                  }
+                  return false;
+              });
+            });
+        };
+    }
+    
     return (
+
+
         <div className={styles.container}>
+
             <main className={styles.main}>
-  
+                
            <div className={styles.grid}>
               <h2>Tutorial</h2>
           </div>     
-    
-  
           <div className="mainpage_content">
               <div className={styles.grid}>
                   <button id="btn">Next</button>
               </div>
             
           </div>
-          <div id="tutorial">
-              <img id="imgg" width="300px" height="300px" ></img>
-             </div>
+          <div id="group">
+          <button id="next">next</button>
+    <div id="divs">
+    <div id="d1">
+    <img  src="imgs/logo/mainlogo.png" />
+            <button className="buttontutorial">
+            Let's Go
+          </button>
+          <button  className="buttontutorial">
+            Tutorial
+          </button>
+          <button className="buttontutorial">
+            About Us
+          </button>
+        </div>
+       
+    <div id="d2">
+            <button className="buttontutorial2">
+            <img  src="imgs/climmates/climate__tropical.png"/>
+            <p>Tropical</p>
+          </button>
+          <button  className="buttontutorial3">
+          <img  src="imgs/climmates/climate__temperate.png"/>
+            <p>Temperate</p>
+          </button>
+          <button className="buttontutorial4">
+          <img  src="imgs/climmates/climate__cold.png"/>
+            <p>Cold</p>
+          </button></div>
+    <div id="d3">
+    <img  src="imgs/tutorials/tutorial_tut_whale.png"/>
+    </div>
+
+</div>
+
+<button id="prev">prev</button>
+</div>     
           </main>
-          
-          <Script  strategy="lazyOnload">
-  {` 
-   document.querySelector('#tutorial').style.display="none";
-  var arr = new collection(['../backgrounds/oceecobg_dark.jpg','../animals/animal__cold_penguine.png','../backgrounds/oceecobg_light.jpg']);
-
-function collection(imgs) {
-  this.imgs = imgs;
-  this.i = 0;
-
-  this.next = function(element) {
-    var img = document.getElementById("imgg");
-    
-    this.i++;
-    if (this.i >= imgs.length) {
-      this.i = 0;
-    }
-
-    img.src = imgs[this.i];
-  };
- 
-  this.prev = function(element) {
-    var img = document.getElementById("imgg");
-  
-    this.i--;
-    if (this.i < 0) {
-      this.i = imgs.length - 1;
-    }
-  
-    img.src = imgs[this.i];
-  };
-  
-  this.next("mainImg"); // to initialize with some image
+          <main>
+        <h1>Nội dung trang chủ</h1>
+      </main>
+<style jsx>
+    {
+    `
+    #d1 {
+      display: grid;
+      place-items:  center;
+      width:400px;
+      }
+      #d1 img{
+        width:40%;
+        margin-bottom:10px;
+      }
+#group{
+    display:flex;
+    position: relative;
+    margin:0;
 }
-      var xDown = null;                                                        
-      var yDown = null;
-      function getTouches(evt) {
-        return evt.touches ||            
-               evt.originalEvent.touches; 
-      }                                                     
-                                                                               
-      function handleTouchStart(evt) {
-          const firstTouch = getTouches(evt)[0];                                      
-          xDown = firstTouch.clientX;                                      
-          yDown = firstTouch.clientY;                                      
-      };                                                
-                                                                               
-      function handleTouchMove(evt) {
-          if ( ! xDown || ! yDown ) {
-              return;
-          }
-          var xUp = evt.touches[0].clientX;                                    
-          var yUp = evt.touches[0].clientY;
-          var xDiff = xDown - xUp;
-          var yDiff = yDown - yUp;                                                      
-          if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-              if ( xDiff > 0 ) {
-               arr.next('imgg');
-              } else {
-                arr.prev('imgg')
-              }                       
-          } else {
-              if ( yDiff > 0 ) {
-              } else { 
-              }                                                                 
-          }
-          xDown = null;
-          yDown = null;                                             
-      };
+.buttontutorial{
+  height: 40px;
+  width: 150px;
+  margin: .5em;
+  display:block;
+  line-height: 40px; 
+  text-align: center;
+  background-color: #CAEBFC;
+  border-radius: 8px;
+  border:0px;
+  box-shadow: 0px 2px 4px rgb(173, 173, 173);
+}
+#d2{
+  display:grid;
+  margin-left: 65px;
+  margin-right:65px;
+}
+.buttontutorial2{
+  width: 250px;
+  height: 80px;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 8px;
+  border: none;
+  background: #FFECA8;
+}
+.buttontutorial2 img {
+  float:left;
+  height: 60px;
+  margin-left:10px;
+}
+.buttontutorial2 p{
+  text-align: center;
+  font-size: 1.5em;
+  margin-right:50px;
+}
+
+.buttontutorial3{
+  width: 250px;
+  height: 80px;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 8px;
+  border: none;
+  background: #91DEA6;
+}
+.buttontutorial3 img {
+  float:left;
+  height: 60px;
+  margin-left:10px;
+}
+.buttontutorial3 p{
+  text-align: center;
+  font-size: 1.5em;
+
+}
+.buttontutorial4{
+  width: 250px;
+  height: 80px;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 8px;
+  border: none;
+  background: #86D4FD;
+}
+.buttontutorial4 img {
+  float:left;
+  height: 60px;
+  margin-left:10px;
+}
+.buttontutorial4 p{
+  text-align: center;
+  font-size: 1.5em;
+  margin-right:70px;
+}
+#d3 img {
+  with:300px;
+  height:250px;
+  margin:20px;
+}
+#prev{
+  margin-top:150px;
+  with:50px;
+  height:50px;
+  color: white;
+  border-radius: 50%;
+}
+#next{
+  margin-top:150px;
+  with:50px;
+  height:50px;
+  color: white;
+  border-radius: 50%;
+}
+
+    `
+    }
+</style>
+
+          <Script  strategy="afterInteractive" >
+  {` 
+      document.querySelector('#group').style.visibility="hidden";
+      document.getElementById("btn").addEventListener("click", remove);
       function remove(){
       document.querySelector('.mainpage_content').style.display="none";;
-      document.querySelector('#tutorial').style.display="block";
-        document.addEventListener('touchstart', handleTouchStart, false);        
-        document.addEventListener('touchmove', handleTouchMove, false);
+      document.querySelector('#group').style.visibility="visible";
       };
-      document.getElementById("btn").addEventListener("click", remove);
+
+   
+    
   `}
            </Script>
       </div>

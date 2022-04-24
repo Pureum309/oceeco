c8a86d49e08f787589d0567bbe82269af38182d6
CanSanchez <asanchez24@my.bcit.ca>
2022-04-23 22:59:26 -0700

    added results page for shark

--- finalapp/pages/questions/results.js
+++ finalapp/pages/questions/results.js
@@ -113,6 +113,56 @@ export default function Qresults(){
 
 {/* SHARK */}
 
+    {
+        an[1] === "shark" && an[2] === "plastic bottle" &&
+        <div className={styles.grid}>
+            <img className={styles.mainlogo} src="./../../imgs/results/result__bottle_shark.png" />
+            
+            <h3 className={styles.heading}><span className={styles.firstcap}>{an[2]}s</span> poke the insides of a {an[1]}'s stomach</h3>
+            
+            <div>
+                <p className={styles.resultsdesc}>
+                Swallowing plastics like bottle caps can hurt the {an[1]} by poking some of their internal organs and not allow them to breathe.
+                </p>
+            </div>
+        </div>
+    }
+
+    {
+        an[1] === "shark" && an[2] === "plastic bag" &&
+        <div className={styles.grid}>
+            <img className={styles.mainlogo} src="./../../imgs/results/result__garbage_shark.png" />
+            
+            <h3 className={styles.heading}><span className={styles.firstcap}>{an[2]}s</span> traps them and causes them to drown</h3>
+            
+            <div>
+                <p className={styles.resultsdesc}>
+                    <span className={styles.firstcap}>{an[1]+"s"}</span> can get trapped in plastic nets or large pile of plastic bags. 
+                    <br></br>
+                    <br></br>
+                    Getting trapped takes them away from their home and family. It can also hurt them badly.
+                </p>
+            </div>
+        </div>
+    }
+
+    {
+        an[1] === "shark" && an[2] === "plastic straw" &&
+        <div className={styles.grid}>
+            <img className={styles.mainlogo} src="./../../imgs/results/result__straw_shark.png" />
+            
+            <h3 className={styles.heading}><span className={styles.firstcap}>{an[2]}s</span> can cause them to starve</h3>
+            
+            <div>
+                <p className={styles.resultsdesc}>
+                Floating plastics like straws easily get cut into small pieces. These might be eaten by the {an[1]}.
+                <br></br>
+                <br></br>
+                They think they're full from eating plastic so they do not get to eat real food then they starve or get hurt inside.
+                </p>
+            </div>
+        </div>
+    }
 
   
     

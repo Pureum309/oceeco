a0105a340094e2b0033a3092d9c16a2d6fb35479
CanSanchez <asanchez24@my.bcit.ca>
2022-05-18 15:11:50 -0700

    updated the about us page

--- finalapp/pages/aboutus.js
+++ finalapp/pages/aboutus.js
@@ -11,78 +11,72 @@ export default function aboutUs() {
     
     return (
     <div className={styles.about}>
-      <div className={styles.container}>
+      <div className={styles.main}>
 
-        <div className={styles.gridab}>
-           <Head>About Us</Head>
-        </div>     
-       
+        <h2 className={styles.heading}>
+            About Us
+        </h2>
 
-        <div className="aboutus_content">
         <div className={styles.grid}>
-            
+            <div className={styles.aboutboxcan}
+            onClick={
+            ()=>r.push("/can")
+            }>
+                <Image 
+                    src="/imgs/about/about__can_seahorse.png" 
+                    alt="Can Seahorse"
+                    width={100}
+                    height={100}
+                />
+                <h3 className={styles.aboutname}>Can</h3>
+                <span className={styles.shadowx}></span>
+            </div>
+
+            <div className={styles.aboutboxsarah}
+            onClick={
+            ()=>r.push("/sarah")
+            }>
+                <Image
+                    src="/imgs/about/about__sarah_whale.png" 
+                    alt="Sarah Whale"
+                    width={100}
+                    height={100}
+                />
+                <h3 className={styles.aboutname}>Sarah</h3>
+                <span className={styles.shadowx}></span>
+            </div>
+
+            <div className={styles.aboutboxkc}
+            onClick={
+            ()=>r.push("/kc")
+            }>
+                <Image 
+                    src="/imgs/about/about__kc_jellyfish.png" 
+                    alt="KC Jellyfish"
+                    width={100}
+                    height={100}
+                />
+                <h3 className={styles.aboutname}>Kaycee</h3>
+                <span className={styles.shadowx}></span>
+            </div>
+
+            <div className={styles.aboutboxhd} 
+            onClick={
+            ()=>r.push("/heidi")
+            }>
+                <Image 
+                    src="/imgs/about/about__heidi_nemo.png" 
+                    alt="Heidi Clownfish"
+                    width={100}
+                    height={100}
+                />
+                <h3 className={styles.aboutname}>Heidi</h3>
+                <span className={styles.shadowx}></span>
+            </div>
         </div>
-
-            <div className={styles.gridcontainer}>
-                <div className={styles.box}>
-                    <button onClick={
-                    ()=>r.push("/can")
-                    }>
-                        <Image 
-                            src="/imgs/about/about__can_seahorse.png" 
-                            alt="Can Seahorse"
-                            width={100}
-                            height={100}
-                        />
-                    </button>
-
-                    <button onClick={
-                    ()=>r.push("/sarah")
-                    }>
-                        <Image
-                            src="/imgs/about/about__sarah_whale.png" 
-                            alt="Sarah Whale"
-                            width={100}
-                            height={100}
-                        />
-                    </button>
-
-                </div>
-            </div>
-
-
-            <div className={styles.gridcontainer}>
-                <div className={styles.box}>
-                    <button onClick={
-                    ()=>r.push("/kc")
-                    }>
-                        <Image 
-                            src="/imgs/about/about__kc_jellyfish.png" 
-                            alt="KC Jellyfish"
-                            width={100}
-                            height={100}
-                        />
-                    </button>
-
-                    <button onClick={
-                    ()=>r.push("/heidi")
-                    }>
-                        <Image 
-                            src="/imgs/about/about__heidi_nemo.png" 
-                            alt="Heidi Clownfish"
-                            width={100}
-                            height={100}
-                        />
-                    </button>
-                </div>
-            </div>
-            
-       
+    </div>
     </div>
 
-
-      </div>
-    
-    </div>
   )
 }
+
